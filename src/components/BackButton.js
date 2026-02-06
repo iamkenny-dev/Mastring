fe39c53ea4d815f703c070b704aae9106cb4ce3a
kenny <adelakunmoyok@gmail.com>
2026-02-06 23:36:59 +0100

    comit

--- src/components/BackButton.js
+++ src/components/BackButton.js
@@ -3,14 +3,14 @@ import React from 'react';
 import { s } from 'react-native-size-matters';
 // import Entypo from '@expo/vector-icons/Entypo';
 // import { Ionicons, MaterialIcons } from '@expo/vector-icons';
-// import Ionicons from 'react-native-vector-icons/Ionicons';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const BackButton = () => {
   return (
     <View style={styles.container}>
-      {/* <Ionicons name="chevron-left" size={24} color="black" /> */}
+      <Ionicons name="chevron-back" size={24} color="black" />
       {/* <Entypo name="chevron-left" size={12} color="black" /> */}
-      <Text>BackButton</Text>
+      {/* <Text>BackButton</Text> */}
     </View>
   );
 };
