c9761646ea0b6d6a7a6abfc651f77af82717b596
kenny <adelakunmoyok@gmail.com>
2026-02-17 20:41:14 +0100

    comit

--- src/screens/ContactUsScreen.js
+++ src/screens/ContactUsScreen.js
@@ -14,7 +14,7 @@ const ContactUsScreen = () => {
         <UserAvatar />
       </View>
       <View style={styles.socialContainer}>
-        <SocialSection icon={<Ionicons name="chevron-back" size={24} color="black" />} title={"Whats up"} />
+        <SocialSection icon={<Ionicons name="chevron-back" size={24} color="black" />} title={"Whatsap"} />
         <SocialSection />
         <SocialSection />
         <SocialSection />
